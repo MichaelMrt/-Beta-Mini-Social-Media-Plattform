8d8f943f7a9313a745f318068d635611bd7be7ca
Michael <michael.martynov03@gmail.com>
2024-10-05 20:20:47 +0200

    feat:backend für login Man wird nun eingeloggt

--- public/auth.js
+++ public/auth.js
@@ -1,6 +1,9 @@
-import { createUserWithEmailAndPassword, updateProfile } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
+import { createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js';
 import { auth } from "./firebase.js";
 
+
+// Create an account with email and password
+if(document.getElementById('registerbutton')!=null){
 document.getElementById('registerbutton').addEventListener("click", () => {
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
@@ -35,5 +38,29 @@ document.getElementById('registerbutton').addEventListener("click", () => {
         // ..
       });
 });
+}
 
 
+// Login in with email and password
+if(document.getElementById('loginbutton')!=null){
+document.getElementById('loginbutton').addEventListener("click", () => {
+  const email = document.getElementById('login-email').value;
+  const password = document.getElementById('login-password').value;
+
+  
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log("Eingeloggt als: "+user.email);
+    window.location.href = "logged_in.html";
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(error);
+    console.log(errorMessage);
+  });
+
+});
+}
