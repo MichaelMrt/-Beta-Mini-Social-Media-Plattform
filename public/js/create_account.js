86e62209ab4c4c98f40b9679f8989180c79d2901
Michael <michael.martynov03@gmail.com>
2024-10-06 18:34:50 +0200

    refactor: create_account.js improve readability

--- public/js/create_account.js
+++ public/js/create_account.js
@@ -6,34 +6,47 @@ document.getElementById('registerbutton').addEventListener("click", () => {
     const username = document.getElementById('username').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-           
-    createUserWithEmailAndPassword(auth, email, password)
+
+    if(!username_exists(username)){
+      createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;
-    
-            try {
-                // Benutzerprofil aktualisieren
-                 updateProfile(user, {
-                    displayName: username
-                })
-                .then(()=>{
-                  console.log("Profil erfolgreich aktualisiert:", user.displayName);
-                  console.log(username);
-                });               
-            } catch (error) {
-                console.error("Fehler beim Aktualisieren des Profils:", error);
-            }
+            updateUserProfileName(user);
+           
             console.log("Erfolgreich registriert");
             console.log(user);
             console.log(user.displayName);
             window.location.href = "/html/logged_in.html";
           })
           .catch((error) => {
+            console.log("Nutzer konnte nicht erstellt werden");
             const errorCode = error.code;
             const errorMessage = error.message;
             console.log(errorCode);
             console.log(errorMessage);
             // ..
-          });
+      });
+    }        
     });
+
+// Benutzerprofil aktualisieren
+function updateUserProfileName(user){
+     updateProfile(user, {
+        displayName: username
+    })
+    .then(()=>{
+      console.log("Profil erfolgreich aktualisiert:", user.displayName);
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("Profil konnte nicht aktualisiert werden");
+        console.log(errorCode);
+        console.log(errorMessage); 
+       }
+  );               
+} 
+
+function username_exists(username){
+  return false;
+}
